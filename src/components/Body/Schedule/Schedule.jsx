5d3570236beedcdc863c27da79007047d2db21d2
goldenchern <jiachernkch@gmail.com>
2025-03-03 23:46:10 +0800

    complete schedule section

--- src/components/Body/Schedule/Schedule.jsx
+++ src/components/Body/Schedule/Schedule.jsx
@@ -4,10 +4,17 @@ function Schedule(){
         <div id='schedule' className='flex flex-col mt-20 items-center'>
             <h1 className='text-[#1E1E1E] uppercase justify-center text-center font-bold md:text-4xl sm:text-3xl text-2xl'>
                 Schedule</h1>
-            <div className='absolute z-[-1] bg-[#C6C6C6] w-1 h-[300px] mt-25 mr-39 '></div>
+            <div className='absolute z-[-1] bg-[#C6C6C6] left-[calc(50%-110px)] transform -translate-x-1/2 w-1 h-[1250px] mt-25 mr-39 '></div>
             <div className='flex flex-col items-center mt-5 w-auto'>
                 <ScheduleCard color='#81BFDA' fontWeight='bold' header='WEEK 1'/>
-                <ScheduleCard color='#B1F0F7' fontWeight='600' header='26 May'/>
+                <ScheduleCard color='#B1F0F7' fontWeight='400' header='26 May' content='Opening Ceremony'/>
+                <ScheduleCard color='#B1F0F7' fontWeight='400' header='27 May' content='HTML/CSS Workshop'/>
+                <ScheduleCard color='#B1F0F7' fontWeight='400' header='28 May' content='React Workshop'/>
+                <ScheduleCard color='#B1F0F7' fontWeight='400' header='29 May' content='Express Workshop'/>
+                <ScheduleCard color='#81BFDA' fontWeight='bold' header='WEEK 2-4' content={`Hacking Period\nCheck-in Sessions`}/>
+                <ScheduleCard color='#81BFDA' fontWeight='bold' header='WEEK 5'/>
+                <ScheduleCard color='#B1F0F7' fontWeight='400' header='26 June' content='Final Hacking Period'/>
+                <ScheduleCard color='#B1F0F7' fontWeight='400' header='27 June' content={`Judging\nClosing Ceremony`}/>
             </div>
         </div>
     )
--- src/components/Body/Schedule/ScheduleCard.jsx
+++ src/components/Body/Schedule/ScheduleCard.jsx
@@ -1,10 +1,10 @@
 function ScheduleCard(props){
     return(
-        <div className='flex h-auto items-center mb-25'>
+        <div className='flex w-full h-auto items-center mb-25'>
             <div className='rounded-4xl w-[200px] h-15 ml-10 text-center flex items-center' style={{ backgroundColor: props.color || 'transparent'}}>
                 <h1 className='text-xl m-auto' style={{fontWeight: props.fontWeight}}>{props.header || 'PlaceHolder'}</h1>
             </div>
-            <p className='ml-30 mr-10'>Test</p>
+            <p className='ml-10 mr-10 whitespace-pre-line italic text-lg'>{props.content || ''}</p>
         </div>
     )
 }
