b3170d4ba9f89b847e56062f53764e7da67474a5
Ngo Zong Han <zonghan01@gmail.com>
2025-03-03 18:32:56 +0800

    mobile navbar

--- src/components/Body/Tagline.jsx
+++ src/components/Body/Tagline.jsx
@@ -3,7 +3,7 @@ import { ReactTyped } from "react-typed";
 function Tagline(){
     return(
         <div className='z-10 text-[#FA6B1C] font-bold md:text-7xl sm:text-6xl text-4xl'>
-            <div className='max-w-[1000px] md:mt-[200px] sm:mt-[75px] mt-[20px] w-screen h-auto mx-auto text-center flex flex-col'>
+            <div className='max-w-[1000px] md:mt-[200px] mt-[75px] w-screen h-auto mx-auto text-center flex flex-col'>
                 <p className=''>BUILD UNDER THE SUN</p>
                 <div className='flex justify-center'>
                     <ReactTyped className='font-bold pl-4'
