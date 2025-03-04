90087bc96d2bf94f920bdba335a94de11915f78c
goldenchern <jiachernkch@gmail.com>
2025-03-04 23:22:23 +0800

    animate schedule section

--- src/components/Body/Schedule/ScheduleCard.jsx
+++ src/components/Body/Schedule/ScheduleCard.jsx
@@ -1,10 +1,10 @@
 function ScheduleCard(props){
     return(
-        <div className='flex w-full h-auto items-center mb-25'>
-            <div className='rounded-4xl w-[200px] h-15 ml-10 text-center flex items-center' style={{ backgroundColor: props.color || 'transparent'}}>
+        <div className='group flex w-full h-auto items-center mb-20'>
+            <div className='rounded-4xl w-[200px] h-15 ml-10 text-center flex items-center transition-all duration-300 ease-in-out transform group-hover:scale-105 group-hover:shadow-lg group-hover:shadow-gray-400' style={{ backgroundColor: props.color || 'transparent'}}>
                 <h1 className='text-xl m-auto' style={{fontWeight: props.fontWeight}}>{props.header || 'PlaceHolder'}</h1>
             </div>
-            <p className='ml-10 mr-10 whitespace-pre-line italic text-lg'>{props.content || ''}</p>
+            <p className='ml-10 mr-10 whitespace-pre-line italic text-lg font-medium duration-400 group-hover:opacity-100 opacity-50'>{props.content || ''}</p>
         </div>
     )
 }
