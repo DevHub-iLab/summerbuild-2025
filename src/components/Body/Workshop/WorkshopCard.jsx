96fb8457a63f67281e7d198c83fc7ab08ea63d16
goldenchern <jiachernkch@gmail.com>
2025-03-05 15:49:39 +0800

    fix workshop card alignment

--- src/components/Body/Workshop/WorkshopCard.jsx
+++ src/components/Body/Workshop/WorkshopCard.jsx
@@ -8,7 +8,7 @@ function WorkshopCard(props) {
     };
 
     return (
-        <div className="flex justify-center items-center perspective-[1000px]">
+        <div className="flex justify-center items-center perspective-[1000px] md:mx-10">
             {/* The actual card with wiggle animation */}
             <div
                 className="rounded-2xl text-center md:w-[250px] sm:w-[225px] w-[200px] 
@@ -29,7 +29,7 @@ function WorkshopCard(props) {
                 {/* Front side */}
                 <div className={`backface-hidden ${!click ? "visible" : "hidden"}`}>
                     <img className="w-90 py-4 px-7" src={props.frontImgSrc} alt="Front" />
-                    <h1 className="text-2xl pt-1 pb-6 px-3 leading-normal">
+                    <h1 className="text-2xl pt-1 pb-6 px-8 leading-normal">
                         {props.frontText || "Default Theme"}
                     </h1>
                 </div>
