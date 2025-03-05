a2abb0016ff035e686066d28f51b68b2d23d193e
Ngo Zong Han <zonghan01@gmail.com>
2025-03-05 14:24:43 +0800

    animations and mobile responsiveness

--- src/components/Body/About/About.jsx
+++ src/components/Body/About/About.jsx
@@ -3,7 +3,7 @@ import AboutCard from "./AboutCard.jsx"
 function About(){
     return(
         <div id='about' className='flex flex-col mt-20'>
-            <h1 className='uppercase justify-center text-center font-bold md:text-4xl sm:text-3xl text-2xl'> 
+            <h1 className='uppercase justify-items-center text-center font-bold md:text-4xl sm:text-3xl text-2xl'> 
                 <span className='text-[#1E1E1E]'>about </span> 
                 <span className='text-[#FA6B1C]'>summerbuild 2025</span>
             </h1>
--- src/components/Body/Sponsor/Sponsor.jsx
+++ src/components/Body/Sponsor/Sponsor.jsx
@@ -2,15 +2,15 @@ import SponsorCard from "./SponsorCard"
 
 function Sponsor(){
     return(
-        <div className='flex flex-col mt-20'>
+        <div className='flex flex-col mt-20 items-center'>
             <h1 className='text-[#1E1E1E] uppercase justify-center text-center font-bold md:text-4xl sm:text-3xl text-2xl'>sponsors</h1>
             <h1 className='text-[#FA6B1C] mt-5 uppercase justify-center text-center font-bold italic md:text-4xl sm:text-3xl text-2xl'>gold sponsors</h1>
-            <div className='flex w-auto justify-center mt-5'>
+            <div className='mt-5 grid grid-cols-1 md:grid-cols-2 gap-5 max-w-[1000px] justify-items-center'>
                 <SponsorCard color='#B1F0F7' text='Sponsor 1'/>
                 <SponsorCard color='#B1F0F7' text='Sponsor 2'/>
             </div>
-            <h1 className='text-[#FA6B1C] mt-10 uppercase justify-center text-center font-bold italic md:text-4xl sm:text-3xl text-2xl'>venue sponsors</h1>
-            <div className='flex w-auto justify-center mt-5'>
+            <h1 className='text-[#FA6B1C] mt-15 uppercase justify-center text-center font-bold italic md:text-4xl sm:text-3xl text-2xl'>venue sponsors</h1>
+            <div className='mt-5 grid grid-cols-1 md:grid-cols-2 gap-5 max-w-[1000px] justify-items-center'>
                 <SponsorCard color='#B1F0F7' text='Sponsor 1'/>
                 <SponsorCard color='#B1F0F7' text='Sponsor 2'/>
             </div>
