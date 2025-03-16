c820eb8586e947520b9b923f8f8e741f0bdaea9b
PuttTim <yannaputt@gmail.com>
2025-03-16 19:15:00 +0800

    ⚡ Update Challenge component layout and enhance ChallengeCard with descriptions

--- src/components/Body/Challenge/Challenge.jsx
+++ src/components/Body/Challenge/Challenge.jsx
@@ -4,14 +4,34 @@ function Challenge() {
     return (
         <div className="mt-20 flex flex-col items-center">
             <h1 className="justify-center text-center text-2xl font-bold uppercase sm:text-3xl md:text-4xl">
-                <span className="text-[#1E1E1E]">Challenges </span>
-                <span className="text-[#FA6B1C]">available</span>
+                <span className="text-[#1E1E1E]">Special </span>
+                <span className="text-[#FA6B1C]">Prizes</span>
             </h1>
-            <div className="mt-3 grid max-w-[1000px] grid-cols-1 justify-items-center gap-5 md:grid-cols-2">
-                <ChallengeCard color="#B1F0F7" text="Best UI/UX" />
-                <ChallengeCard color="#FADA7A" text="Most Innovative" />
-                <ChallengeCard color="#F5F0CD" text="Most Useful" />
-                <ChallengeCard color="#81BFDA" text="Best Freshmen Themes" />
+            <h3 className="mt-3 text-center text-lg font-semibold text-[#1E1E1E]">
+                Challenge yourself with these special categories and win
+                exciting prizes!
+            </h3>
+            <div className="mt-3 grid max-w-[90vw] auto-rows-auto grid-cols-1 justify-items-center gap-2 sm:max-w-[80vw] lg:grid-cols-2 lg:gap-5 xl:max-w-[60vw]">
+                <ChallengeCard
+                    color="#B1F0F7"
+                    text="Best UI/UX"
+                    description="Create an intuitive user experience."
+                />
+                <ChallengeCard
+                    color="#FADA7A"
+                    text="Most Innovative"
+                    description="Innovate something completely new."
+                />
+                <ChallengeCard
+                    color="#f7b1c1"
+                    text="Most Useful"
+                    description="Build a practical solution."
+                />
+                <ChallengeCard
+                    color="#f7d7e1"
+                    text="Best Freshmen Team"
+                    description="Your entire team are all Year 1 students."
+                />
             </div>
         </div>
     )
--- src/components/Body/Challenge/ChallengeCard.jsx
+++ src/components/Body/Challenge/ChallengeCard.jsx
@@ -1,12 +1,17 @@
+import { FlaskConical } from "lucide-react"
+
 function ChallengeCard(props) {
     return (
         <div
-            className="m-2 h-30 w-[325px] transform rounded-md text-center transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg hover:shadow-gray-400 sm:w-[350px]"
-            style={{ backgroundColor: props.color || "transparent" }}
-        >
-            <h1 className="mt-1 p-2 font-bold">
-                {props.text || "Default Challenge"}
-            </h1>
+            className="col-span-1 grid h-full w-full grid-cols-4 rounded-lg py-2"
+            style={{ backgroundColor: props.color }}>
+            <div className="col-span-1 flex h-full w-full items-center justify-center px-2 py-4 lg:px-0">
+                <FlaskConical size={48} color="#FA6B1C" />
+            </div>
+            <div className="col-span-3 flex w-full flex-col items-start justify-around pr-4">
+                <h1 className="text-xl font-bold">{props.text}</h1>
+                <h2 className="text-lg">{props.description}</h2>
+            </div>
         </div>
     )
 }
