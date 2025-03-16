b6392e021a2d33fd918c65a6f3ee8e1778b216bc
PuttTim <yannaputt@gmail.com>
2025-03-16 19:16:49 +0800

    ⚡Added Sponsor information

--- src/components/Body/Sponsor/SponsorCard.jsx
+++ src/components/Body/Sponsor/SponsorCard.jsx
@@ -2,13 +2,12 @@ function SponsorCard(props) {
     return (
         <div
             className="transform rounded-md py-1 text-center transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg hover:shadow-gray-400"
-            style={{ backgroundColor: props.color || "transparent" }}
-        >
+            style={{ backgroundColor: props.color || "transparent" }}>
             <h1 className="px-10 py-7 text-xl font-semibold">
                 {props.text || "Default Theme"}
             </h1>
             <img
-                className="mx-auto mb-7 h-[150px] w-[150px] rounded-lg object-cover"
+                className="mx-auto mb-7 h-[150px] rounded-lg object-cover"
                 src={props.imgSrc}
                 alt="Logo"
             />
