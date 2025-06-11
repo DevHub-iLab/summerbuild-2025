872a1b239e3c88086fe96117629edcb8ad4d9b43
PuttTim <yannaputt@gmail.com>
2025-06-12 07:41:00 +0800

    ⚡ Updated Countdown

--- src/components/Body/Countdown/Countdown.jsx
+++ src/components/Body/Countdown/Countdown.jsx
@@ -6,8 +6,8 @@ function Countdown() {
         <div className="flex w-auto flex-col items-center pt-10">
             {/*26 May 2025, 10am*/}
             <Timer
-                timer="Start Building In..."
-                targetDate="2025-05-26T10:00:00"
+                timer="Final Submission in.."
+                targetDate="2025-06-26T11:00:00"
             />
         </div>
     )
