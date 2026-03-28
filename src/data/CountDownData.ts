6298d992e80aeb6171b3d205ab3656d476cda953
Ngo Zong Han <zonghan01@gmail.com>
2026-03-28 21:16:29 +0800

    updated reg link

--- src/data/CountDownData.ts
+++ src/data/CountDownData.ts
@@ -5,5 +5,5 @@ export const countdownData = {
     "Don’t miss your chance to join SummerBuild '26. Secure your spot before registration closes!",
   deadline: "2026-05-18T23:59:59+08:00", // change this
   buttonText: "Register Now",
-  buttonLink: "https://forms.microsoft.com/", // replace with your real Microsoft Form
+  buttonLink: "https://forms.office.com/r/fTgy59yWq1", // replace with your real Microsoft Form
 };
